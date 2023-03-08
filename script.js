7b3d302d24fbe40553a5dbb1f2a3ea1c65bbd153
Shuffle10 <saphal.pant@westcliff.edu>
2023-03-08 20:19:50 +0545

    Added audio files

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ let userChoices = document.getElementsByClassName("btn")
 let options = ['rock', 'paper', 'scissors'];
 let message = document.getElementById("messageBox")
 
+let winAudio = new Audio("win.mp3")
+let loseAudio = new Audio("lose.mp3")
+let drawAudio = new Audio("draw.mp3")
+
+
 for(let e of Array.from(userChoices)){
     e.addEventListener("click", ()=>{
         let userChoice =  e.innerHTML;
@@ -10,26 +15,31 @@ for(let e of Array.from(userChoices)){
 
         if (userChoice.toLowerCase()=='rock' && computerChoice == 'scissors' ){
             message.style.color = "green";
+            winAudio.play();
             message.innerHTML = `Computer chose ${computerChoice}! You won!`;   
         }
 
         else if (userChoice.toLowerCase()=='paper' && computerChoice == 'rock' ){
             message.style.color = "green";
+            winAudio.play();
             message.innerHTML = `Computer chose ${computerChoice}! You won!`;       
         }
 
         else if (userChoice.toLowerCase()=='scissors' && computerChoice == 'paper' ){
             message.style.color = "green";
+            winAudio.play();
             message.innerHTML = `Computer chose ${computerChoice}! You won!`;      
         }
 
         else if (userChoice.toLowerCase()==computerChoice){
             message.style.color = "orange";
+            drawAudio.play();
             message.innerHTML = `Computer chose ${computerChoice}! It's a draw!`;      
         }
 
         else{
             message.style.color = "red";
+            loseAudio.play();
             message.innerHTML = `Computer chose ${computerChoice}! You lose!`; 
         }
 
